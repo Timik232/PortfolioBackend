ff63aae433b2e7a152eff585e22e0acfa98aecea
Timur Komolov <komolov.timurka@mail.ru>
2025-01-09 02:08:56 +0300

    version v2 with dockerfiles

---
+++ PictureMe/portfolio/admin.py
@@ -0,0 +1,57 @@
+# from django import forms
+from django.contrib import admin
+
+from portfolio.models import Album, Description, Element, Image, Order
+
+# class MultiplyImageForm(forms.ModelForm):
+#     class Meta:
+#         model = Image
+#         fields = "__all__"
+#
+#         widgets = {
+#             'images': forms.FileField(widget=
+#             forms.ClearableFileInput(attrs={'multiple': True}))
+#         }
+
+
+admin.site.register(Album)
+admin.site.register(Element)
+admin.site.register(Image)
+admin.site.register(Description)
+# admin.site.register(Order)
+
+
+class ImageInline(admin.StackedInline):
+    model = Image
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    fields = (
+        "name",
+        "phone_number",
+        "email",
+        "vk_url",
+        "is_vk_answer",
+        "purpose",
+        "message",
+    )
+    list_display = ["get_time"]
+    list_filter = ["get_time", "purpose", "is_vk_answer"]
+    search_fields = ["purpose", "get_time", "is_vk_answer"]
+
+
+# @admin.register(Image)
+# class ImageAdmin(admin.ModelAdmin):
+#     fields = ("none",)
+#     inlines = [ImageInline, ]
+
+
+# @admin.register(Image)
+# class ElementAdmin(admin.ModelAdmin):
+# inlines = [ImageInline, ]
+
+
+# @admin.register(Element)
+# class ElementAdmin(admin.ModelAdmin):
+# inlines = [ImageInline, ]
---
+++ superuser.py
@@ -0,0 +1,37 @@
+import os
+from pathlib import Path
+
+import django
+import dotenv
+from django.contrib.auth.models import User
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+dotenv_file = os.path.join(BASE_DIR, os.environ.get("ENV_FILE", ".env"))
+if os.path.isfile(dotenv_file):
+    dotenv.load_dotenv(dotenv_file)
+
+# Установка переменной окружения, указывающей на файл с настройками Django
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "PictureMe.settings")
+django.setup()
+
+# Импорт модели суперпользователя Django
+
+
+# Функция для создания суперпользователя, если его ещё нет
+def create_superuser_if_not_exists():
+    username = os.environ.get("DJANGO_SUPERUSER_USERNAME", "lumpen")
+    email = os.environ.get("DJANGO_SUPERUSER_EMAIL", "otrizanie@gmail.com")
+    password = os.environ.get("DJANGO_SUPERUSER_PASSWORD", "not_udar")
+
+    try:
+        if not User.objects.filter(username=username).exists():
+            user = User.objects.create_superuser(username, email, password)
+            print(f"Superuser {user.username} created successfully!")
+        else:
+            print("Superuser already exists.")
+    except Exception as e:
+        print(f"Error creating superuser: {e}")
+
+
+if __name__ == "__main__":
+    create_superuser_if_not_exists()
