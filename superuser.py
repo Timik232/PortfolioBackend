8a3b18e8d0de958e7252ea9afd5415d5e58b32a2
Timur Komolov <komolov.timurka@mail.ru>
2025-01-18 00:19:57 +0300

    superuser must be changed

--- superuser.py
+++ superuser.py
@@ -1,27 +1,24 @@
 import os
-from pathlib import Path
-
 import django
+from pathlib import Path
 import dotenv
-from django.contrib.auth.models import User
+
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-dotenv_file = os.path.join(BASE_DIR, os.environ.get("ENV_FILE", ".env"))
+dotenv_file = os.path.join(BASE_DIR, os.environ.get("ENV_FILE", '.env'))
 if os.path.isfile(dotenv_file):
     dotenv.load_dotenv(dotenv_file)
 
 # Установка переменной окружения, указывающей на файл с настройками Django
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "PictureMe.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PictureMe.settings')
 django.setup()
 
-# Импорт модели суперпользователя Django
-
-
+from django.contrib.auth.models import User
 # Функция для создания суперпользователя, если его ещё нет
 def create_superuser_if_not_exists():
-    username = os.environ.get("DJANGO_SUPERUSER_USERNAME", "lumpen")
-    email = os.environ.get("DJANGO_SUPERUSER_EMAIL", "otrizanie@gmail.com")
-    password = os.environ.get("DJANGO_SUPERUSER_PASSWORD", "not_udar")
+    username = os.environ.get('DJANGO_SUPERUSER_USERNAME', "lumpen")
+    email = os.environ.get('DJANGO_SUPERUSER_EMAIL', "otrizanie@gmail.com")
+    password = os.environ.get('DJANGO_SUPERUSER_PASSWORD', "not_udar")
 
     try:
         if not User.objects.filter(username=username).exists():
